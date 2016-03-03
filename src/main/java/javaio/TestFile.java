946ab330e75af4781bcb1c01e5fa9453856b8913
Jack Shen <jack.shen@tpgtelecom.com.au>
2016-03-03 14:17:19 +1100

    Add example of file.list

--- src/main/java/javaio/TestFile.java
+++ src/main/java/javaio/TestFile.java
@@ -1,9 +1,40 @@
 package javaio;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class TestFile {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		//listFile();
+		listFileByName("\\..*"); //list all files start with '.'
 	}
+	
+	
+	private static void listFile() {
+		File path = new File(".");
+		String[] list = path.list();
+		Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
+		for (String dirItem : list) {
+			System.out.println(dirItem);
+		}
+	}
+
+	private static void listFileByName(String regex) {
+		File path = new File(".");
+		String[] list = path.list(new FilenameFilter() {
+			private Pattern p = Pattern.compile(regex);
+			public boolean accept(File dir, String name) {
+				return p.matcher(name).matches();
+			}
+		});
+		Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
+		for (String dirItem : list) {
+			System.out.println(dirItem);
+		}
+	}
+
 
 }
